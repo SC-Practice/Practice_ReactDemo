93832d790a0752ceeba37dca374e87b92295d40b
sc3l <sean.z.chen@newegg.com>
2017-03-21 16:39:55 +0800

    add SyntaxJSX view

--- ReactDemo/Controllers/HomeController.cs
+++ ReactDemo/Controllers/HomeController.cs
@@ -23,5 +23,10 @@ namespace ReactDemo.Controllers
         {
             return View();
         }
+
+        public ActionResult SyntaxJSX()
+        {
+            return View();
+        }
     }
 }
