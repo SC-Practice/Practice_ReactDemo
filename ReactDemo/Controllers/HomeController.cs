b795264b83ff9d6106e0790f81500d12ed5ece8b
sc3l <sean.z.chen@newegg.com>
2017-03-24 13:30:02 +0800

    use props to send data to component

--- ReactDemo/Controllers/HomeController.cs
+++ ReactDemo/Controllers/HomeController.cs
@@ -1,32 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ReactDemo.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => View();
 
-        public ActionResult IndexJSXWithBabel() 
-        {
-            return View();
-        }
+        public ActionResult IndexJSXWithBabel() => View();
 
-        public ActionResult IndexEs5AndEs6()
-        {
-            return View();
-        }
+        public ActionResult IndexEs5AndEs6() => View();
 
-        public ActionResult SyntaxJSX()
-        {
-            return View();
-        }
+        public ActionResult SyntaxJSX() => View();
     }
 }
